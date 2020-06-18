b64e79ce3ffa3ebe3761ba31c619adef1c1c6dab
cqdlhjpdml <535914126@qq.com>
2020-06-19 00:15:19 +0800

    Revert "Revert "Revert "2020－06－18"""
    This reverts commit a6aa02d0616e0a67cea830e56385cf7709f46e2f.

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const ws  = require('./routes/ws-routes.js');
 const app = new Koa();
 
 app.use(staicServer(path.join(__dirname,Config.staticpath)));
-const editordemo = (ctx) => {
-  ctx.response.body = pug.renderFile(path.join(__dirname,Config.viewpath)+'/editordemo.pug');
-  console.log(ctx.response.body);
-};
 const main = (ctx) => {
   ctx.response.body = pug.renderFile(path.join(__dirname,Config.viewpath)+'/index.pug');
   console.log(ctx.response.body);
@@ -22,7 +18,6 @@ const index= (ctx) => {
 };
 router.get("/",index);
 router.get("/demo.html",main);
-router.get("/editordemo.html",editordemo);
 const getDir=(ctx,next)=>{
   console.log(ctx);next();
 }
