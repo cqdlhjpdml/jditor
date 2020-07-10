b42ed49fa9180db7007062986fdd228270d5b546
cqdlhjpdml <535914126@qq.com>
2020-07-10 15:56:35 +0800

    2020－07－10

--- debug.js
+++ debug.js
@@ -6,30 +6,34 @@ const Config = require('./server-config.js');
 const pug = require('pug');
 const ws  = require('./routes/ws-routes.js');
 const app = new Koa();
+const fs = require('fs');
+const srcpath='/src/js';
+const viewspath='/views'
 
 app.use(staicServer(path.join(__dirname,Config.staticpath)));
-const main = (ctx) => {
-  ctx.response.body = pug.renderFile(path.join(__dirname,Config.viewpath)+'/index.pug');
-  console.log(ctx.response.body);
-};
-const index= (ctx) => {
-  ctx.response.body = pug.renderFile(path.join(__dirname,Config.viewpath)+'/index.html');
-  console.log(ctx.response.body);
-};
-router.get("/",index);
-router.get("/demo.html",main);
-const getDir=(ctx,next)=>{
-  console.log(ctx);next();
-}
 
-const jmain = (ctx) => {
-  ctx.response.body = pug.renderFile(path.join(__dirname,Config.viewpath)+'/jdemo.pug');
-  console.log(ctx.response.body);
-};
-router.get("/jdemo.html",jmain);
+app.use(async (ctx, next) => {
+  
+  if (ctx.request.path.indexOf('.js')!=-1) { // get js files
+    ctx.response.body = fs.readFileSync(path.join(__dirname,srcpath,ctx.request.path), 'utf8');
+    ctx.response.status = 200
+    ctx.response.type="text/plain"
+    console.log('js file sent')
+    console.log(ctx)
+  } else 
+  if (ctx.request.path === '/debug.html') { // 列表页
+    ctx.response.status = 200;
+    ctx.response.type="string";
+    ctx.response.body = fs.readFileSync(path.join(__dirname,viewspath,ctx.request.path), 'utf8');
+    console.log(ctx)
+  } else {
+    ctx.throw(404, 'Not found') // 404
+  }
+//await next()
+})
 
 var port=Config.httpPort;
-app.use(getDir);
+
 app.use(router.routes());
 app.listen(port);
 console.log('listening on port:'+port);
